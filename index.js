5665306232278d3f2018ea81265ce6ddb86c729a
Daniel <dano.husar@gmail.com>
2014-08-24 18:39:44 +0100

    relative urls

--- index.js
+++ index.js
@@ -2,31 +2,33 @@
 var gutil = require('gulp-util');
 var through = require('through2');
 var fs = require('fs');
-var files = [];
 
 module.exports = function (options) {
 
+  var files = [];
+
   options = options ? options : {};
   options.filename = options.filename || 'output.json';
+  options.relative = options.relative || false;
   options.strip = options.strip || false;
 
   return through.obj(function (file, enc, cb) {
-
-    var path = file.path;
-    if (options.strip) {
-      path = path.replace(options.strip, '');
-    }
-    files.push(path);
-
-    if (file.isNull()) {
-      this.push(file);
-      return cb();
-    }
 
     if (file.isStream()) {
       this.emit('error', new gutil.PluginError('gulp-to-json', 'Streaming not supported'));
       return cb();
     }
+
+    var path = file.path;
+    if (options.relative) {
+      path = file.relative;
+    }
+
+    if (options.strip) {
+      path = path.replace(options.strip, '');
+    }
+
+    files.push(path);
 
     this.push(file);
     return cb();
--- test.js
+++ test.js
@@ -1,10 +1,12 @@
 'use strict';
-var should = require('should');
-var gutil = require('gulp-util');
-var toJson = require('./index');
-var fs = require('fs');
 
-describe('It should store data', function(){
+var fs = require('fs');
+var gutil = require('gulp-util');
+var should = require('should');
+var toJson = require('./index');
+
+
+describe('It should store data', function () {
 
   it('Store sample data', function (cb) {
 
@@ -12,11 +14,36 @@ describe('It should store data', function(){
       strip: /^.+\/?\\?public\/?\\?/
     });
 
-    stream.on('data', function(){});
+    stream.on('data', function () {});
 
-    stream.on('end', function(){
+    stream.on('end', function () {
       var data = require('./output.json');
       data[0].should.equal('index.html');
+      cb();
+    });
+
+    stream.write(new gutil.File({
+      base: __dirname,
+      path: __dirname + '/public/index.html',
+      contents: new Buffer('unicorns')
+    }));
+
+    stream.end();
+
+  });
+
+  it('Store sample data with relative url', function (cb) {
+
+    var stream = toJson({
+      relative: true,
+      filename: 'output2.json'
+    });
+
+    stream.on('data', function () {});
+
+    stream.on('end', function () {
+      var data = require('./output2.json');
+      data[0].should.equal('public/index.html');
       cb();
     });
 
